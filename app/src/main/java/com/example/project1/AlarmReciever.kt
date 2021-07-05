84676c1a33c886039efa4eb91656577a8d167a69
lke1221 <lke001221@gmail.com>
2021-07-05 16:55:25 +0900

    0705 16:55

---
+++ app/src/main/java/com/example/project1/AlarmReciever.kt
@@ -0,0 +1,15 @@
+package com.example.project1
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+
+
+    }
+
+
+}
